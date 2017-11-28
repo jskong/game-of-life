480f53a18bf52eea1aaea30b3413125a66cd73a1
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2017-11-27 21:03:01 -0800

    Implement runTurn to run one turn of the game of life

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,5 +1,5 @@
 -- test matrices
-let m1 = [[False, True, True],[True, True, True],[False, False, False]]
+m1 = [[False, True, True],[True, True, True],[False, False, False]]
 
 
 -- Conway's Game of life
@@ -8,21 +8,28 @@ play matrix 0 = matrix
 play matrix numTurns = play (runTurn matrix) (numTurns - 1)
 
 runTurn :: [[Bool]] -> [[Bool]]
-runTurn matrix = []
-  -- for each cell c0 in the matrix:
-  -- count the number of live neighbours n
-  -- then applyRules c0 n
+runTurn matrix =
+  map (\(yIndex, row) ->
+    map (\(xIndex, cellStatus) ->
+      applyRules cellStatus (countAliveAdjacents matrix xIndex yIndex))
+    (enumerate row))
+  (enumerate matrix)
 
-applyRules :: Bool -> Int -> Int
+applyRules :: Bool -> Int -> Bool
 applyRules c0 n =
-  if c0 == True
-  then if (n == 3)
-       then 1
-       else 0
-  else if (n == 2 || n == 3)
-       then 1
-       else 0
+  if c0
+  then if (n == 2 || n == 3)
+       then True
+       else False
+  else if (n == 3)
+       then True
+       else False
 
+countAliveAdjacents :: [[Bool]] -> Int -> Int -> Int
 countAliveAdjacents oMatrix x y = length (filter
   (\(x, y) -> (y >= 0  && y < (length oMatrix) && x >= 0 && x < (length (oMatrix!!0)) && oMatrix!!y!!x))
   [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)])
+
+
+-- Helpers
+enumerate = zip [0..]
