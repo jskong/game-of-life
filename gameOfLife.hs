51c6ee09d8b760db3125b4f58563dfadde414ff0
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2017-11-27 23:16:07 -0800

    Improve naming of helpers and variables, make isValidCoordOf work for jagged matrices

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -12,24 +12,20 @@ runTurn matrix =
   map (\(yIndex, row) ->
     map (\(xIndex, cellStatus) ->
       applyRules cellStatus (countAliveAdjacents matrix xIndex yIndex))
-    (enumerate row))
-  (enumerate matrix)
+    (zipWithIndex row))
+  (zipWithIndex matrix)
 
 applyRules :: Bool -> Int -> Bool
-applyRules c0 n =
-  if c0
-  then if (n == 2 || n == 3)
-       then True
-       else False
-  else if (n == 3)
-       then True
-       else False
+applyRules cellStatus numAliveAdj =
+  if cellStatus
+  then (numAliveAdj == 2 || numAliveAdj == 3)
+  else (numAliveAdj == 3)
 
 countAliveAdjacents :: [[Bool]] -> Int -> Int -> Int
-countAliveAdjacents oMatrix x y = length (filter (isValidCoord oMatrix) (adjacentCoordsOf x y))
+countAliveAdjacents oMatrix x y = length (filter (isValidAliveCoordOf oMatrix) (adjacentCoordsOf x y))
 
 
 -- Helpers
-enumerate = zip [0..]
+zipWithIndex = zip [0..]
 adjacentCoordsOf x y = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
-isValidCoord matrix = (\(x, y) -> (y >= 0  && y < (length matrix) && x >= 0 && x < (length (matrix!!0)) && matrix!!y!!x))
+isValidAliveCoordOf matrix = (\(x, y) -> (y >= 0  && y < (length matrix) && x >= 0 && x < (length (matrix!!y)) && matrix!!y!!x))
