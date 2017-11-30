89da3ee122718098a78f5dbbffd55d04d4b69e4c
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2017-11-29 18:09:59 -0800

    Improve naming, add types, reorganize

--- boardGenerator.ts
+++ boardGenerator.ts
@@ -8,6 +8,8 @@ let generateBoard = (x, y) => {
     let values = []
     for (let j = 0; j < x; j++) {
       let rand = Math.floor(Math.random() * 10) + 1
+      // Change the int below to anything between 1-10. This is the chance
+      // that the value will be true. Int = 6 => True = 60%, False = 40%
       values.push(rand < 6)
     }
     matrix[i] = values
